68eaf1c7cae8c0b85cb0f3e31b807d6d736b9a03
毛欢欢 <517612110@qq.com>
2021-05-06 13:50:17 +0800

    补充说明

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # 工程简介
 
+## 参考地址
+### 依赖包:
+[1. Docker中搭建FastDFS文件系统（多图）](https://www.cnblogs.com/niceyoo/p/13511082.html)
 
+[2. SpringBoot集成FastDFS依赖实现文件上传](https://www.cnblogs.com/niceyoo/p/13512089.html)
 
-# 延伸阅读
-
-# fastdfs-demo
--- src/main/java/com/example/demo/config/ComponetImport.java
+++ src/main/java/com/example/demo/config/ComponetImport.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.EnableMBeanExport;
 import org.springframework.context.annotation.Import;
 import org.springframework.jmx.support.RegistrationPolicy;
 
+/**
+ * @author maohh
+ */
 @Configuration
 @Import(FdfsClientConfig.class)
 @EnableMBeanExport(registration = RegistrationPolicy.IGNORE_EXISTING)
